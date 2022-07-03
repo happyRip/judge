3c6859da519d0111144e0fb10a5d102074bf0735
happyRip <aleksander.borowski.01@gmail.com>
2022-07-03 22:45:43 +0200

    Use generics in sum

--- sum/sum.go
+++ sum/sum.go
@@ -1,7 +1,23 @@
 package sum
 
-func Sum(args ...int) int {
-	var result int
+type integer interface {
+	int | int16 | int32 | int64
+}
+
+type unsigned interface {
+	uint | uint16 | uint32 | uint64
+}
+
+type floating interface {
+	float32 | float64
+}
+
+type number interface {
+	integer | unsigned | floating
+}
+
+func Sum[T number](args ...T) T {
+	var result T
 	for _, v := range args {
 		result += v
 	}
